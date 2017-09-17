a81d2b3614cd69337019655995ff6f7acc2e418a
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-09-17 09:48:05 -0500

    loop over test chunks

--- wrapper/erd/generate-input/GenRepDev.sh
+++ wrapper/erd/generate-input/GenRepDev.sh
@@ -24,60 +24,53 @@ echo train.arff has been generated
 
 echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
 
-read -p "enter which chunk folder for the test-file : "  chunkname
+#read -p "enter which chunk folder for the test-file : "  chunkname
 
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-echo deleting previous files in nltkdata test folders 
-find ./nltk_data/corpora/eRisk/test -maxdepth 2 -type f -delete 
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+declare -a arr=("chunk_1" "chunk1-2" "chunk1-2-3" "chunk1-2-3-4" "chunk1-2-3-4-5" "chunk1-2-3-4-5-6" "chunk1-2-3-4-5-6-7" "chunk1-2-3-4-5-6-7-8" "chunk1-2-3-4-5-6-7-8-9" "chunk1-2-3-4-5-6-7-8-9-10")
 
-echo now moving test files 
-python ./move-testfiles/generate_test_data.py ../development-dataset/ttdev-test ./nltk_data/corpora/eRisk/test $chunkname
+for chunkname in "${arr[@]}"
+do
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+	echo deleting previous files in nltkdata test folders 
+	find ./nltk_data/corpora/eRisk/test -maxdepth 2 -type f -delete 
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-
-echo finished moving testfiles
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-
-echo running featurespacetree train and test
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
-
-#python ./featurespacetree/entry_point.py pyramidal-soa2.yaml --train
-#python ./featurespacetree/entry_point.py pyramidal-soa2.yaml --test
-
-python ./featurespacetree/entry_point.py settings_soa_GOOD.yaml --train
-python ./featurespacetree/entry_point.py settings_soa_GOOD.yaml --test
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-
-echo labeling the testing arff_file
-
-echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-if [ $chunkname = "chunk_1" ]
-then 
-    newname="chunk1"
-    
-    python ./labeling/label_arff.py ./labeling/train_golden_truth.txt ./representation_files/weka_exp_0/test_subspaceRoot_weka_exp_0.arff ./test_$newname.arff 
+	echo now moving test files 
+	python ./move-testfiles/generate_test_data.py ../development-dataset/ttdev-test ./nltk_data/corpora/eRisk/test "$chunkname"
 
 	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
 
-	echo test_$newname.arff has been generated 
-
-	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
-else
-	python ./labeling/label_arff.py ./labeling/train_golden_truth.txt ./representation_files/weka_exp_0/test_subspaceRoot_weka_exp_0.arff ./test_$chunkname.arff
+	echo finished moving testfiles
 
 	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
 
-	echo test_$chunkname.arff has been generated 
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+
+	echo running featurespacetree train and test
 
 	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
-fi
+
+	#python ./featurespacetree/entry_point.py pyramidal-soa2.yaml --train
+	#python ./featurespacetree/entry_point.py pyramidal-soa2.yaml --test
+
+	python ./featurespacetree/entry_point.py settings_soa_GOOD.yaml --train
+	python ./featurespacetree/entry_point.py settings_soa_GOOD.yaml --test
+
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+
+	echo labeling the testing arff_file
+
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    python ./labeling/label_arff.py ./labeling/train_golden_truth.txt ./representation_files/weka_exp_0/test_subspaceRoot_weka_exp_0.arff ./test_"$chunkname".arff
+
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+
+	echo test_"$chunkname".arff has been generated 
+
+	echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     
+done
+
 
 
 
