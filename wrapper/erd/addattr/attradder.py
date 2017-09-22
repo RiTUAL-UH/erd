1e11372f5a809608e42acd7d4c9c66b2ffc9df74
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-09-21 19:32:56 -0500

    dict for id + need to fix premature eof error

--- wrapper/erd/addattr/attradder.py
+++ wrapper/erd/addattr/attradder.py
@@ -2,7 +2,10 @@ import argparse
 import os
 
 """ adds attributes from readscorer's output to arff files 
-viz input arffs to erd-system produced by feature space tree"""
+viz input arffs to erd-system produced by feature space tree 
+
+NOTE: for now manually add @attrs in output file as it is weird to do it in a single pass 
+"""
 
 def get_attrs(attr_file):
 
@@ -11,14 +14,36 @@ def get_attrs(attr_file):
 		attrs = { item[0] : (item[1],item[2]) for item in id_fog_lwf}
 		return attrs
 
-def add_attrs(attrs,arff_file):
-	pass 
+def add_attrs(attrs,arff_file,output_file):
+
+	with open(arff_file) as af:
+		data_flag = False
+		with open(output_file,'w') as out:
+
+			for line in af:
+				
+				pieces = line.split()
+
+				if data_flag:
+					fog_lwf = attrs[pieces[-1]]
+					pieces.insert(-2,fog_lwf[0]+',')
+					pieces.insert(-2,fog_lwf[1]+',')
+					out.write(' '.join(s for s in pieces))
+					out.write('\n')
+				else:
+					out.write(line)
+
+				if pieces[0] == '@data':
+					data_flag = True
+
+	return
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description='USAGE :   python attradder.py --attr <> --arff <> ')
+	parser = argparse.ArgumentParser(description='USAGE :   python attradder.py --attr <> --arff <> --out <> ')
 	parser.add_argument('-a','--attr',help='path to ids and attributes  file',required=True)
-	parser.add_argument('-b','--arff',help='path to arff  file',required=True)	
+	parser.add_argument('-b','--arff',help='path to arff  file',required=True)
+	parser.add_argument('-o','--out',help='output file name',required=True)	
 	args= vars(parser.parse_args())
 
-	print get_attrs(args['attr'])
+	add_attrs(get_attrs(args['attr']),args['arff'],args['out'])
